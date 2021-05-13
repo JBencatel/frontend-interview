e0d664adf26e17ee6abed855295a337935c8b741
Joana Bencatel <bencateljoana@gmail.com>
2021-05-13 20:33:19 +0100

    Create the basic structure of project Used Gulp to make it easier to compile/build the simplified versrion

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify')
+const concat = require('gulp-concat')
+const sass = require('gulp-sass')
+
+gulp.task("html", () => {
+    return gulp.src("src/*.html")
+        .pipe(gulp.dest("build"))
+})
+
+gulp.task('scripts', () => {
+    return gulp.src('src/js/*.js').pipe(concat('main.js')).pipe(uglify()).pipe(gulp.dest('build/js'));
+})
+
+gulp.task('fonts', () => {
+    return gulp.src('src/assets/fonts/*').pipe(gulp.dest('build/fonts'));
+})
+
+gulp.task('sass', () => {
+    return gulp.src('src/sass/*.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('build/css'));
+})
+
+gulp.task('default', gulp.series('html', 'scripts', 'fonts', 'sass'));
+
+gulp.task('watch', () => {
+    gulp.watch('src/*.html', gulp.series('html'));
+    gulp.watch('src/js/*.js', gulp.series('scripts'));
+    gulp.watch('src/sass/*.scss', gulp.series('sass'));
+})
