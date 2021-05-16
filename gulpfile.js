4ef8fb02dad4cee693974c3226ecc4b780a8d213
Joana Bencatel <bencateljoana@gmail.com>
2021-05-16 07:59:51 +0100

    Add documentation to the gulfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,17 @@ const concat = require('gulp-concat')
 const babel = require('gulp-babel');
 const sass = require('gulp-sass')
 
+/**
+ * Copies all development html files to the build folder
+ */
 gulp.task("html", () => {
 	return gulp.src("src/*.html")
 		.pipe(gulp.dest("build"))
 })
 
+/**
+ * Transpiles and concatenates all development js files into a single main.js file, minifies it and includes it in the build folder.
+ */
 gulp.task('scripts', () => {
 	return gulp
 		.src('src/js/*.js')
@@ -23,21 +29,36 @@ gulp.task('scripts', () => {
 		.pipe(gulp.dest('build/js'));
 })
 
+/**
+ * Copies the images in the development folder, minifies them and places than in the build folder.
+ */
 gulp.task('images', () => {
 	return gulp.src('src/assets/images/*')
 		.pipe(imagemin()).pipe(gulp.dest('build/images'));
 });
 
+/**
+ *  Copies the images from the development folder to the build folder.
+ */
 gulp.task('fonts', () => {
 	return gulp.src('src/assets/fonts/*').pipe(gulp.dest('build/fonts'));
 })
 
+/**
+ * Transpiles the development scss files and adds the now css files to the build folder.
+ */
 gulp.task('sass', () => {
 	return gulp.src('src/sass/*.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('build/css'));
 })
 
+/**
+ * Builds/compiles the entire application.
+ */
 gulp.task('default', gulp.series('html', 'scripts', 'images', 'fonts', 'sass'));
 
+/**
+ * Watches for changes in the development html, js and scss files to update the build folder.
+ */
 gulp.task('watch', () => {
 	gulp.watch('src/*.html', gulp.series('html'));
 	gulp.watch('src/js/*.js', gulp.series('scripts'));
